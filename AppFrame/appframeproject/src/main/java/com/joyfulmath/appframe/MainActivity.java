147a3e5687180091926c39ad125db79520d3afa7
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-03-22 16:58:33 +0800

    init project

---
+++ AppFrame/appframeproject/src/main/java/com/joyfulmath/appframe/MainActivity.java
@@ -0,0 +1,9 @@
+package com.joyfulmath.appframe;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+}
