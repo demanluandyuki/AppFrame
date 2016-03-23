bfbc36a4a3f0fd4043e8499a62b5a1e478275fca
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-03-23 17:54:46 +0800

    add code

---
+++ AppFrame/appframeproject/src/main/java/com/joyfulmath/appframe/business/Test.java
@@ -0,0 +1,8 @@
+package com.joyfulmath.appframe.business;
+
+/**
+ * @author deman.lu
+ * @version on 2016-03-23 16:09
+ */
+public class Test {
+}
---
+++ AppFrame/appframeproject/src/main/java/com/joyfulmath/appframe/business/map/view/MapActivity.java
@@ -0,0 +1,10 @@
+package com.joyfulmath.appframe.business.map.view;
+
+import android.app.Activity;
+
+/**
+ * @author deman.lu
+ * @version on 2016-03-23 17:41
+ */
+public class MapActivity extends Activity {
+}
