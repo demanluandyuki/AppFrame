3f92b9eff0e6e0eb9ef0ec62f46bb8672004a4c6
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-03-24 11:38:06 +0800

    add acitivity

--- AppFrame/appframeproject/src/main/java/com/joyfulmath/appframe/business/map/view/MapActivity.java
+++ AppFrame/appframeproject/src/main/java/com/joyfulmath/appframe/business/map/view/MapActivity.java
@@ -1,10 +1,27 @@
 package com.joyfulmath.appframe.business.map.view;
 
 import android.app.Activity;
+import android.widget.TextView;
+
+import com.joyfulmath.appframe.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * @author deman.lu
  * @version on 2016-03-23 17:41
  */
+@EActivity(R.layout.activity_map)
 public class MapActivity extends Activity {
+
+    @ViewById(R.id.tv_show)
+    TextView    mTvShow;
+
+    @AfterViews
+    void afterview()
+    {
+
+    }
 }
